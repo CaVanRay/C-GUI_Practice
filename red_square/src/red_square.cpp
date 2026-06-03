530be288d5cd5439769b533625f9fa8c2ff3f278
Cavan Theiss <cavanrt@gmail.com>
2026-06-03 08:17:26 -0500

    Update red_square.cpp

--- red_square/src/red_square.cpp
+++ red_square/src/red_square.cpp
@@ -1,5 +1,10 @@
 #include <SDL2/SDL.h>
+#include <SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 #include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
 
 int main() {
 
@@ -7,6 +12,26 @@ int main() {
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0){
+        std::cerr << "SDL_mixer couldnot initialize! Error: " << Mix_GetError() << std::endl;
+        return 1;
+    }
+
+    Mix_Chunk* jumpSound = NULL;
+    Mix_Music* bgMusic = NULL;
+
+    jumpSound = Mix_LoadWAV("assets/jump.wav");
+    if(jumpSound == NULL){
+        std::cerr << "Failed to load jump sound! SDL_mixer Error: " << Mix_GetError() << std::endl;
+        return 1;
+    }
+
+    bgMusic = Mix_LoadMUS("assets/backgroundMusic.wav");
+    if(bgMusic == NULL){
+        std::cerr << "Failed to load background Music! SDL_mixer Error: " << Mix_GetError() << std::endl;
         return 1;
     }
 
